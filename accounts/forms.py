d11621aba3cf50af4a71afe9e628e00c1c4590f5
Wayne Peng <junwen.wayne.peng@gmail.com>
2023-11-16 23:10:46 +0800

    dark-light css adjust

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser as User
-
+from crispy_forms.layout import Layout, Fieldset, Submit
+from crispy_forms.helper import FormHelper
 
 class SignUpForm(UserCreationForm):
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
@@ -14,5 +15,6 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['student_id', 'first_name', 'last_name', 'nick_name', 'gender', 'department', 'email']
+
 
 
