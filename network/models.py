4a2b59f587de1425560a3d9f8580e0c06eb5f4ee
wayne peng <junwen.wayne.peng@gmail.com>
2023-11-17 12:59:04 +0800

    update

--- network/models.py
+++ network/models.py
@@ -2,33 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
-class Relation(models.TextChoices):
-	friend = "friend", "Friend"
-	discussion = "discussion", "Discussion"
-	learn = "following", "I learn from "
-	teach = "followed", "I taught"
-
 class Status(models.TextChoices):
 	pending = "pending", "pending"
 	confirm = "confirm", "confirm"
 
 class NotificationVerbs(models.TextChoices):
-	friend_request = "friend", "friend request"
-	discussion_request = "discussion", "discussion request"
 	following_request = "following", "following request"
-	followed_request = "followed", "followed request"
 	confirm = "confirm", "confirm of your request"
 	update = "cvupdate", "information update notification"
-
 
 class FriendRequest(models.Model):
 	sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_requests')
 	receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_requests')
-	relationship_type = models.CharField(max_length=20, choices=Relation.choices, default="friend")
 	status = models.CharField(max_length=10, choices=Status.choices, default='pending')
 	timestamp = models.DateTimeField(auto_now_add=True)
 
 class Relationship(models.Model):
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='relationships')
-	friend = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	relationship_type = models.CharField(max_length=20, choices=Relation.choices)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_relationships')
+	friend = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='friend_relationships')
+	timestamp = models.DateTimeField(auto_now_add=True)
--- network/views.py
+++ network/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from accounts.models import CustomUser as User
 from django.db.models import Q
-from .models import Relationship, FriendRequest, Relation, NotificationVerbs
+from .models import Relationship, FriendRequest, NotificationVerbs
 from notifications.signals import notify
 from django.db.models.signals import post_save
 from django.contrib import messages
@@ -42,12 +42,12 @@ def home(request):
         else:
             old_edges = set(edges)
             for edge in old_edges:
-                if (edge[0], edge[3]) in source:
-                    source.add((edge[1], edge[4]))
+                if (edge[0], edge[1]) in source:
+                    source.add((edge[2], edge[3]))
                     relationships.add(edge)
                     edges.remove(edge)
-                if (edge[1], edge[4]) in source:
-                    source.add((edge[0], edge[3]))
+                if (edge[2], edge[3]) in source:
+                    source.add((edge[0], edge[1]))
                     relationships.add(edge)
                     edges.remove(edge)
             return find_nodes_within_distance(source, relationships, edges, length-1)
@@ -67,16 +67,15 @@ def home(request):
 
         # Add edges for relationships
         for relation in relationships:
-            G.add_edge(relation.user.id, relation.friend.id, label=relation.relationship_type)
+            G.add_edge(relation.user.id, relation.friend.id)
     else:
-        edges = {(relation.user.id, relation.friend.id, relation.relationship_type, relation.user.username, relation.friend.username) for relation in relationships}
+        edges = {(relation.user.id, relation.user.username, relation.friend.id, relation.friend.username) for relation in relationships}
         source, local_relationships = find_nodes_within_distance({(user.id, user.username)}, set(), edges, 2)
-        print(source, local_relationships)
         try:
             for node in source:
                 G.add_node(node[0], label=node[1])
             for relation in local_relationships:
-                G.add_edge(relation[0], relation[1], label=relation[2])
+                G.add_edge(relation[0], relation[2])
         except Exception as e:
             print(e)
 
@@ -88,43 +87,54 @@ def home(request):
 
 def search_users(request):
     query = request.GET.get('q', '')
-    
+    user=request.user
     # Perform the search based on the query.
-    results = User.objects.filter(Q(username__icontains=query) | Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(nick_name__icontains=query) | Q(email__icontains=query))
-    return render(request, 'search_results.html', {'results': results, 'query': query, 'choices': Relation.choices})
+    results = User.objects.filter(Q(username__icontains=query) | Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(nick_name__icontains=query) | Q(email__icontains=query) | Q(student_id__icontains=query)).exclude(id=user.id)
+    request_has_sent = list()
+    friends = list()
+    for result in results:
+        if FriendRequest.objects.filter(sender=user.id, receiver=result.id).first():
+            request_has_sent.append(result)
+        if Relationship.objects.filter(user=user.id, friend=result.id).first():
+            friends.append(result)
+        print(friends)
+    return render(
+            request, 
+            'search_results.html', 
+            {
+                'results': results, 
+                'query': query, 
+                'request_has_sent': request_has_sent,
+                'friends': friends,
+                'current_page': request.path_info
+                })
 
 def send_friend_request(request, user_id):
-    if request.method == 'POST':
+    if request.method=='POST':
         # Get the selected relationship type from the form
-        relationship_type = request.POST.get('relationship')
-        notification_desicription = {choice[0]: choice[1] for choice in NotificationVerbs.choices}
-        notification_verbs = lambda relation_type: 'connection_request' if relationship_type in [notif[0] for notif in NotificationVerbs.choices[0:3]] else relationship_type
-        if relationship_type:
-            # Create a friend request with the selected relationship type
-            # Get the sender and receiver users
-            sender = request.user
-            receiver = User.objects.get(id=user_id)
+        # Create a friend request with the selected relationship type
+        # Get the sender and receiver users
+        sender = request.user
+        receiver = User.objects.get(id=user_id)
 
-            # Check if a friend request already exists
-            friend_request = FriendRequest.objects.filter(
-                sender=sender, receiver=receiver, relationship_type=relationship_type
-            ).first()
-            if not friend_request:
-                # Create a new friend request
-                friend_request = FriendRequest(
-                    sender=sender, receiver=receiver, relationship_type=relationship_type
+        # Check if a friend request already exists
+        friend_request = FriendRequest.objects.filter(
+            sender=sender, receiver=receiver
+        ).first()
+        if not friend_request:
+            # Create a new friend request
+            friend_request = FriendRequest(
+                sender=sender, receiver=receiver
+            )
+            friend_request.save()
+            notify.send(
+                sender, 
+                recipient=receiver, 
+                verb=f'following',
+                description=f'{sender} send you a following request',
+                action_object=friend_request
                 )
-                friend_request.save()
-                print(notification_verbs(relationship_type))
-                notify.send(
-                    sender, 
-                    recipient=receiver, 
-                    verb=f'{notification_verbs(relationship_type)}',
-                    description=f'{sender} send you a {notification_desicription[relationship_type]}',
-                    action_object=friend_request
-                    )
-
-    return redirect('home')
+    return HttpResponseRedirect(request.path_info)
 
 def confirm_request(request, notification_id):
     notification = request.user.notifications.get(id=notification_id)	
@@ -137,14 +147,8 @@ def confirm_request(request, notification_id):
     Relationship.objects.create(
         user=request.user,
         friend=friend_request.sender,
-        relationship_type=friend_request.relationship_type,
     )
-
-
-    # Display a success message
-    messages.success(request, 'Friend request confirmed.')
     # Redirect to the desired page after confirmation
-
     return HttpResponseRedirect(request.path_info)
 
 def ignore_request(request, notification_id):
