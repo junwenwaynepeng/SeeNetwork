d25c465ea8791511bfdb003b0929f7607f9e7d29
Wayne Peng <junwen.wayne.peng@gmail.com>
2023-11-20 18:53:41 +0800

    update

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, WorkExperience, SelfIntroduction, EssentialSkill, Award, Publication
 
 
 class CustomUserAdmin(UserAdmin):
@@ -19,4 +19,30 @@ class CustomUserAdmin(UserAdmin):
     }),
 )
 
+
+class WorkExperienceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'position', 'company', 'start_date', 'end_date')
+    search_fields = ('user__username', 'position', 'company')
+
+class SelfIntroductionAdmin(admin.ModelAdmin):
+    list_display = ('user', )
+    search_fields = ('user__username', )
+
+class EssentialSkillAdmin(admin.ModelAdmin):
+    list_display = ('user', 'skill_name')
+    search_fields = ('user__username', 'skill_name')
+
+class AwardAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'year')
+    search_fields = ('user__username', 'title')
+
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'authors', 'publication_date')
+    search_fields = ('user__username', 'title', 'authors')
+
+admin.site.register(WorkExperience, WorkExperienceAdmin)
+admin.site.register(SelfIntroduction, SelfIntroductionAdmin)
+admin.site.register(EssentialSkill, EssentialSkillAdmin)
+admin.site.register(Award, AwardAdmin)
+admin.site.register(Publication, PublicationAdmin)
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,41 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser as User
 from crispy_forms.layout import Layout, Fieldset, Submit
 from crispy_forms.helper import FormHelper
+from .models import WorkExperience, SelfIntroduction, EssentialSkill, Award, Publication, SelfDefinedContent
+
+class WorkExperienceForm(forms.ModelForm):
+    class Meta:
+        model = WorkExperience
+        fields = ['position', 'company', 'start_date', 'end_date', 'description']
+
+class SelfIntroductionForm(forms.ModelForm):
+    class Meta:
+        model = SelfIntroduction
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        }
+
+
+class EssentialSkillForm(forms.ModelForm):
+    class Meta:
+        model = EssentialSkill
+        fields = ['skill_name']
+
+class AwardForm(forms.ModelForm):
+    class Meta:
+        model = Award
+        fields = ['title', 'year']
+
+class PublicationForm(forms.ModelForm):
+    class Meta:
+        model = Publication
+        fields = ['title', 'authors', 'publication_date']
+
+class SelfDefinedContent(forms.ModelForm):
+    class Meta:
+        model = SelfDefinedContent
+        fields = ['title', 'content']
 
 class SignUpForm(UserCreationForm):
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
@@ -15,6 +50,4 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['student_id', 'first_name', 'last_name', 'nick_name', 'gender', 'department', 'email']
-
-
 
